001c597e98dd7483684f6fbf0ad2cdddd347c670
Tim Graupmann <tgraupmann@gmail.com>
2019-01-17 10:33:22 -0800

    Replace public ints with enums

--- Converter.cs
+++ Converter.cs
@@ -214,9 +214,24 @@ namespace ChromaAPISync
             return true;
         }
 
-        private static bool GetArgsTypes(string args, List<MetaArgInfo> detailArgs)
+        public class MetaOverride
         {
-            string[] parts = args.Split(",".ToCharArray());
+            public string MethodName = string.Empty;
+            public string ArgName = string.Empty;
+            public string OverrideType = string.Empty;
+            public string CastType = string.Empty;
+        }
+
+        private static readonly MetaOverride[] _sUnityOverrides =
+        {
+            new MetaOverride() { MethodName="GetMaxLeds", ArgName="device", OverrideType="Device1D", CastType="int"},
+            new MetaOverride() { MethodName="GetMaxRow", ArgName="device", OverrideType="Device2D", CastType="int"},
+            new MetaOverride() { MethodName="GetMaxColumn", ArgName="device", OverrideType="Device2D", CastType="int"},
+        };
+
+        private static bool GetArgsTypes(MetaMethodInfo methodInfo)
+        {
+            string[] parts = methodInfo.Args.Split(",".ToCharArray());
             for (int i = 0; i < parts.Length; ++i)
             {
                 string part = parts[i].Trim();
@@ -227,9 +242,19 @@ namespace ChromaAPISync
                     string name = part.Substring(indexName + 1).Trim();
 
                     MetaArgInfo info = new MetaArgInfo();
+                    info.MethodInfo = methodInfo;
                     info.StrType = strType;
                     info.Name = name;
-                    detailArgs.Add(info);
+                    foreach (MetaOverride metaOverride in _sUnityOverrides)
+                    {
+                        if (info.MethodInfo.Name == metaOverride.MethodName &&
+                        info.Name == metaOverride.ArgName)
+                        {
+                            info.OverrideType = metaOverride.OverrideType;
+                            info.CastType = metaOverride.CastType;
+                        }
+                    }
+                    methodInfo.DetailArgs.Add(info);
                 }
             }
             return true;
@@ -368,9 +393,9 @@ namespace ChromaAPISync
             return returnStr.TrimEnd();
         }
 
-        private static string RemoveArgTypes(string args, List<MetaArgInfo> detailArgs)
+        private static string RemoveArgTypes(MetaMethodInfo methodInfo)
         {
-            string[] parts = args.Split(",".ToCharArray());
+            string[] parts = methodInfo.Args.Split(",".ToCharArray());
             for (int i = 0; i < parts.Length; ++i)
             {
                 string part = parts[i].TrimEnd();
@@ -378,11 +403,16 @@ namespace ChromaAPISync
                 if (indexName > 0)
                 {
                     string name = part.Substring(indexName + 1);
-                    if (detailArgs[i].StrType == "const char*" ||
-                        detailArgs[i].StrType == "char*")
+                    MetaArgInfo argInfo = methodInfo.DetailArgs[i];
+                    if (argInfo.StrType == "const char*" ||
+                        argInfo.StrType == "char*")
                     {
                         string lpArg = string.Format("lp{0}", UppercaseFirstLetter(name));
                         name = lpArg;
+                    }
+                    if (!string.IsNullOrEmpty(argInfo.OverrideType))
+                    {
+                        name = string.Format("({0}){1}", argInfo.CastType, name);
                     }
                     if (i > 0)
                     {
@@ -547,9 +577,9 @@ namespace ChromaAPISync
             return result;
         }
 
-        private static string ChangeArgsToManagedTypes(string args)
+        private static string ChangeArgsToManagedTypes(MetaMethodInfo methodInfo)
         {
-            string[] parts = args.Split(",".ToCharArray());
+            string[] parts = methodInfo.Args.Split(",".ToCharArray());
             for (int i = 0; i < parts.Length; ++i)
             {
                 string part = parts[i].Trim();
@@ -559,6 +589,11 @@ namespace ChromaAPISync
                     string strType = part.Substring(0, indexName + 1).Trim();
                     strType = ChangeToManagedType(strType);
                     string name = part.Substring(indexName + 1).Trim();
+                    MetaArgInfo argInfo = methodInfo.DetailArgs[i];
+                    if (!string.IsNullOrEmpty(argInfo.OverrideType))
+                    {
+                        strType = argInfo.OverrideType;
+                    }
 
                     if (i == 0)
                     {
@@ -602,8 +637,11 @@ namespace ChromaAPISync
         
         class MetaArgInfo
         {
+            public MetaMethodInfo MethodInfo;
             public string Name = string.Empty;
             public string StrType = string.Empty;
+            public string CastType = string.Empty;
+            public string OverrideType = string.Empty;
         }
 
         class MetaMethodInfo
@@ -699,7 +737,7 @@ namespace ChromaAPISync
                             }
                             //Console.WriteLine("Args: {0}", methodInfo.Args);
 
-                            if (!GetArgsTypes(methodInfo.Args, methodInfo.DetailArgs))
+                            if (!GetArgsTypes(methodInfo))
                             {
                                 continue;
                             }
@@ -988,6 +1026,187 @@ namespace ChromaSDK
         const string DLL_NAME = ""UnityNativeChromaSDK"";
 #endif
 
+        #region Data Structures
+
+        public enum DeviceType
+        {
+            Invalid = -1,
+            DE_1D = 0,
+            DE_2D = 1,
+            MAX = 2,
+        }
+
+        public enum Device
+        {
+            Invalid = -1,
+            ChromaLink = 0,
+            Headset = 1,
+            Keyboard = 2,
+            Keypad = 3,
+            Mouse = 4,
+            Mousepad = 5,
+            MAX = 6,
+        }
+
+        public enum Device1D
+        {
+            Invalid = -1,
+            ChromaLink = 0,
+            Headset = 1,
+            Mousepad = 2,
+            MAX = 3,
+        }
+
+        public enum Device2D
+        {
+            Invalid = -1,
+            Keyboard = 0,
+            Keypad = 1,
+            Mouse = 2,
+            MAX = 3,
+        }
+
+        private static Dictionary<KeyCode, int> _sKeyMapping = null;
+
+        public static int GetKeyboardRazerKey(KeyCode keyCode)
+        {
+            if (_sKeyMapping == null)
+            {
+                _sKeyMapping = new Dictionary<KeyCode, int>();
+                _sKeyMapping[KeyCode.Backspace] = (int)Keyboard.RZKEY.RZKEY_BACKSPACE;
+                _sKeyMapping[KeyCode.Tab] = (int)Keyboard.RZKEY.RZKEY_TAB;
+                _sKeyMapping[KeyCode.Return] = (int)Keyboard.RZKEY.RZKEY_ENTER;
+                _sKeyMapping[KeyCode.Pause] = (int)Keyboard.RZKEY.RZKEY_PAUSE;
+                _sKeyMapping[KeyCode.Escape] = (int)Keyboard.RZKEY.RZKEY_ESC;
+                _sKeyMapping[KeyCode.Space] = (int)Keyboard.RZKEY.RZKEY_SPACE;
+                _sKeyMapping[KeyCode.Exclaim] = (int)Keyboard.RZKEY.RZKEY_1;
+                _sKeyMapping[KeyCode.DoubleQuote] = (int)Keyboard.RZKEY.RZKEY_OEM_8;
+                _sKeyMapping[KeyCode.Hash] = (int)Keyboard.RZKEY.RZKEY_3;
+                _sKeyMapping[KeyCode.Dollar] = (int)Keyboard.RZKEY.RZKEY_4;
+                _sKeyMapping[KeyCode.Ampersand] = (int)Keyboard.RZKEY.RZKEY_7;
+                _sKeyMapping[KeyCode.Quote] = (int)Keyboard.RZKEY.RZKEY_OEM_8;
+                _sKeyMapping[KeyCode.LeftParen] = (int)Keyboard.RZKEY.RZKEY_9;
+                _sKeyMapping[KeyCode.RightParen] = (int)Keyboard.RZKEY.RZKEY_0;
+                _sKeyMapping[KeyCode.Asterisk] = (int)Keyboard.RZKEY.RZKEY_8;
+                _sKeyMapping[KeyCode.Plus] = (int)Keyboard.RZKEY.RZKEY_OEM_3;
+                _sKeyMapping[KeyCode.Comma] = (int)Keyboard.RZKEY.RZKEY_OEM_9;
+                _sKeyMapping[KeyCode.Minus] = (int)Keyboard.RZKEY.RZKEY_OEM_2;
+                _sKeyMapping[KeyCode.Period] = (int)Keyboard.RZKEY.RZKEY_OEM_10;
+                _sKeyMapping[KeyCode.Slash] = (int)Keyboard.RZKEY.RZKEY_OEM_11;
+                _sKeyMapping[KeyCode.Alpha0] = (int)Keyboard.RZKEY.RZKEY_0;
+                _sKeyMapping[KeyCode.Alpha1] = (int)Keyboard.RZKEY.RZKEY_1;
+                _sKeyMapping[KeyCode.Alpha2] = (int)Keyboard.RZKEY.RZKEY_2;
+                _sKeyMapping[KeyCode.Alpha3] = (int)Keyboard.RZKEY.RZKEY_3;
+                _sKeyMapping[KeyCode.Alpha4] = (int)Keyboard.RZKEY.RZKEY_4;
+                _sKeyMapping[KeyCode.Alpha5] = (int)Keyboard.RZKEY.RZKEY_5;
+                _sKeyMapping[KeyCode.Alpha6] = (int)Keyboard.RZKEY.RZKEY_6;
+                _sKeyMapping[KeyCode.Alpha7] = (int)Keyboard.RZKEY.RZKEY_7;
+                _sKeyMapping[KeyCode.Alpha8] = (int)Keyboard.RZKEY.RZKEY_8;
+                _sKeyMapping[KeyCode.Alpha9] = (int)Keyboard.RZKEY.RZKEY_9;
+                _sKeyMapping[KeyCode.Colon] = (int)Keyboard.RZKEY.RZKEY_OEM_7;
+                _sKeyMapping[KeyCode.Semicolon] = (int)Keyboard.RZKEY.RZKEY_OEM_7;
+                _sKeyMapping[KeyCode.Less] = (int)Keyboard.RZKEY.RZKEY_OEM_9;
+                _sKeyMapping[KeyCode.Equals] = (int)Keyboard.RZKEY.RZKEY_OEM_3;
+                _sKeyMapping[KeyCode.Greater] = (int)Keyboard.RZKEY.RZKEY_OEM_10;
+                _sKeyMapping[KeyCode.Question] = (int)Keyboard.RZKEY.RZKEY_OEM_11;
+                _sKeyMapping[KeyCode.At] = (int)Keyboard.RZKEY.RZKEY_2;
+                _sKeyMapping[KeyCode.LeftBracket] = (int)Keyboard.RZKEY.RZKEY_OEM_4;
+                _sKeyMapping[KeyCode.Backslash] = (int)Keyboard.RZKEY.RZKEY_OEM_6;
+                _sKeyMapping[KeyCode.RightBracket] = (int)Keyboard.RZKEY.RZKEY_OEM_5;
+                _sKeyMapping[KeyCode.Caret] = (int)Keyboard.RZKEY.RZKEY_6;
+                _sKeyMapping[KeyCode.Underscore] = (int)Keyboard.RZKEY.RZKEY_OEM_2;
+                _sKeyMapping[KeyCode.BackQuote] = (int)Keyboard.RZKEY.RZKEY_OEM_1;
+                _sKeyMapping[KeyCode.A] = (int)Keyboard.RZKEY.RZKEY_A;
+                _sKeyMapping[KeyCode.B] = (int)Keyboard.RZKEY.RZKEY_B;
+                _sKeyMapping[KeyCode.C] = (int)Keyboard.RZKEY.RZKEY_C;
+                _sKeyMapping[KeyCode.D] = (int)Keyboard.RZKEY.RZKEY_D;
+                _sKeyMapping[KeyCode.E] = (int)Keyboard.RZKEY.RZKEY_E;
+                _sKeyMapping[KeyCode.F] = (int)Keyboard.RZKEY.RZKEY_F;
+                _sKeyMapping[KeyCode.G] = (int)Keyboard.RZKEY.RZKEY_G;
+                _sKeyMapping[KeyCode.H] = (int)Keyboard.RZKEY.RZKEY_H;
+                _sKeyMapping[KeyCode.I] = (int)Keyboard.RZKEY.RZKEY_I;
+                _sKeyMapping[KeyCode.J] = (int)Keyboard.RZKEY.RZKEY_J;
+                _sKeyMapping[KeyCode.K] = (int)Keyboard.RZKEY.RZKEY_K;
+                _sKeyMapping[KeyCode.L] = (int)Keyboard.RZKEY.RZKEY_L;
+                _sKeyMapping[KeyCode.M] = (int)Keyboard.RZKEY.RZKEY_M;
+                _sKeyMapping[KeyCode.N] = (int)Keyboard.RZKEY.RZKEY_N;
+                _sKeyMapping[KeyCode.O] = (int)Keyboard.RZKEY.RZKEY_O;
+                _sKeyMapping[KeyCode.P] = (int)Keyboard.RZKEY.RZKEY_P;
+                _sKeyMapping[KeyCode.Q] = (int)Keyboard.RZKEY.RZKEY_Q;
+                _sKeyMapping[KeyCode.R] = (int)Keyboard.RZKEY.RZKEY_R;
+                _sKeyMapping[KeyCode.S] = (int)Keyboard.RZKEY.RZKEY_S;
+                _sKeyMapping[KeyCode.T] = (int)Keyboard.RZKEY.RZKEY_T;
+                _sKeyMapping[KeyCode.U] = (int)Keyboard.RZKEY.RZKEY_U;
+                _sKeyMapping[KeyCode.V] = (int)Keyboard.RZKEY.RZKEY_V;
+                _sKeyMapping[KeyCode.W] = (int)Keyboard.RZKEY.RZKEY_W;
+                _sKeyMapping[KeyCode.X] = (int)Keyboard.RZKEY.RZKEY_X;
+                _sKeyMapping[KeyCode.Y] = (int)Keyboard.RZKEY.RZKEY_Y;
+                _sKeyMapping[KeyCode.Z] = (int)Keyboard.RZKEY.RZKEY_Z;
+                _sKeyMapping[KeyCode.Delete] = (int)Keyboard.RZKEY.RZKEY_DELETE;
+                _sKeyMapping[KeyCode.Keypad0] = (int)Keyboard.RZKEY.RZKEY_NUMPAD0;
+                _sKeyMapping[KeyCode.Keypad1] = (int)Keyboard.RZKEY.RZKEY_NUMPAD1;
+                _sKeyMapping[KeyCode.Keypad2] = (int)Keyboard.RZKEY.RZKEY_NUMPAD2;
+                _sKeyMapping[KeyCode.Keypad3] = (int)Keyboard.RZKEY.RZKEY_NUMPAD3;
+                _sKeyMapping[KeyCode.Keypad4] = (int)Keyboard.RZKEY.RZKEY_NUMPAD4;
+                _sKeyMapping[KeyCode.Keypad5] = (int)Keyboard.RZKEY.RZKEY_NUMPAD5;
+                _sKeyMapping[KeyCode.Keypad6] = (int)Keyboard.RZKEY.RZKEY_NUMPAD6;
+                _sKeyMapping[KeyCode.Keypad7] = (int)Keyboard.RZKEY.RZKEY_NUMPAD7;
+                _sKeyMapping[KeyCode.Keypad8] = (int)Keyboard.RZKEY.RZKEY_NUMPAD8;
+                _sKeyMapping[KeyCode.Keypad9] = (int)Keyboard.RZKEY.RZKEY_NUMPAD9;
+                _sKeyMapping[KeyCode.KeypadPeriod] = (int)Keyboard.RZKEY.RZKEY_NUMPAD_DECIMAL;
+                _sKeyMapping[KeyCode.KeypadDivide] = (int)Keyboard.RZKEY.RZKEY_NUMPAD_DIVIDE;
+                _sKeyMapping[KeyCode.KeypadMultiply] = (int)Keyboard.RZKEY.RZKEY_NUMPAD_MULTIPLY;
+                _sKeyMapping[KeyCode.KeypadMinus] = (int)Keyboard.RZKEY.RZKEY_NUMPAD_SUBTRACT;
+                _sKeyMapping[KeyCode.KeypadPlus] = (int)Keyboard.RZKEY.RZKEY_NUMPAD_ADD;
+                _sKeyMapping[KeyCode.KeypadEnter] = (int)Keyboard.RZKEY.RZKEY_NUMPAD_ENTER;
+                _sKeyMapping[KeyCode.UpArrow] = (int)Keyboard.RZKEY.RZKEY_UP;
+                _sKeyMapping[KeyCode.DownArrow] = (int)Keyboard.RZKEY.RZKEY_DOWN;
+                _sKeyMapping[KeyCode.RightArrow] = (int)Keyboard.RZKEY.RZKEY_RIGHT;
+                _sKeyMapping[KeyCode.LeftArrow] = (int)Keyboard.RZKEY.RZKEY_LEFT;
+                _sKeyMapping[KeyCode.Insert] = (int)Keyboard.RZKEY.RZKEY_INSERT;
+                _sKeyMapping[KeyCode.Home] = (int)Keyboard.RZKEY.RZKEY_HOME;
+                _sKeyMapping[KeyCode.End] = (int)Keyboard.RZKEY.RZKEY_END;
+                _sKeyMapping[KeyCode.PageUp] = (int)Keyboard.RZKEY.RZKEY_PAGEUP;
+                _sKeyMapping[KeyCode.PageDown] = (int)Keyboard.RZKEY.RZKEY_PAGEDOWN;
+                _sKeyMapping[KeyCode.F1] = (int)Keyboard.RZKEY.RZKEY_F1;
+                _sKeyMapping[KeyCode.F2] = (int)Keyboard.RZKEY.RZKEY_F2;
+                _sKeyMapping[KeyCode.F3] = (int)Keyboard.RZKEY.RZKEY_F3;
+                _sKeyMapping[KeyCode.F4] = (int)Keyboard.RZKEY.RZKEY_F4;
+                _sKeyMapping[KeyCode.F5] = (int)Keyboard.RZKEY.RZKEY_F5;
+                _sKeyMapping[KeyCode.F6] = (int)Keyboard.RZKEY.RZKEY_F6;
+                _sKeyMapping[KeyCode.F7] = (int)Keyboard.RZKEY.RZKEY_F7;
+                _sKeyMapping[KeyCode.F8] = (int)Keyboard.RZKEY.RZKEY_F8;
+                _sKeyMapping[KeyCode.F9] = (int)Keyboard.RZKEY.RZKEY_F9;
+                _sKeyMapping[KeyCode.F10] = (int)Keyboard.RZKEY.RZKEY_F10;
+                _sKeyMapping[KeyCode.F11] = (int)Keyboard.RZKEY.RZKEY_F11;
+                _sKeyMapping[KeyCode.F12] = (int)Keyboard.RZKEY.RZKEY_F12;
+                _sKeyMapping[KeyCode.Numlock] = (int)Keyboard.RZKEY.RZKEY_NUMLOCK;
+                _sKeyMapping[KeyCode.CapsLock] = (int)Keyboard.RZKEY.RZKEY_CAPSLOCK;
+                _sKeyMapping[KeyCode.ScrollLock] = (int)Keyboard.RZKEY.RZKEY_SCROLL;
+                _sKeyMapping[KeyCode.RightShift] = (int)Keyboard.RZKEY.RZKEY_RSHIFT;
+                _sKeyMapping[KeyCode.LeftShift] = (int)Keyboard.RZKEY.RZKEY_LSHIFT;
+                _sKeyMapping[KeyCode.RightControl] = (int)Keyboard.RZKEY.RZKEY_RCTRL;
+                _sKeyMapping[KeyCode.LeftControl] = (int)Keyboard.RZKEY.RZKEY_LCTRL;
+                _sKeyMapping[KeyCode.RightAlt] = (int)Keyboard.RZKEY.RZKEY_RALT;
+                _sKeyMapping[KeyCode.LeftAlt] = (int)Keyboard.RZKEY.RZKEY_LALT;
+                _sKeyMapping[KeyCode.LeftWindows] = (int)Keyboard.RZKEY.RZKEY_LWIN;
+                _sKeyMapping[KeyCode.Print] = (int)Keyboard.RZKEY.RZKEY_PRINTSCREEN;
+                _sKeyMapping[KeyCode.SysReq] = (int)Keyboard.RZKEY.RZKEY_PRINTSCREEN;
+                _sKeyMapping[KeyCode.Break] = (int)Keyboard.RZKEY.RZKEY_PAUSE;
+                _sKeyMapping[KeyCode.Menu] = (int)Keyboard.RZKEY.RZKEY_RMENU;
+            }
+            if (_sKeyMapping.ContainsKey(keyCode))
+            {
+                return _sKeyMapping[keyCode];
+            }
+            else
+            {
+                return (int)Keyboard.RZKEY.RZKEY_INVALID;
+            }
+        }
+
+        #endregion
+
         #region Helpers (handle path conversions)
 
         /// <summary>
@@ -1062,7 +1281,7 @@ namespace ChromaSDK
                     Output(swUnity, "\t\tpublic static {0} {1}({2})",
                         ChangeToManagedType(methodInfo.ReturnType),
                         methodInfo.Name,
-                        ChangeArgsToManagedTypes(methodInfo.Args));
+                        ChangeArgsToManagedTypes(methodInfo));
 
                     Output(swUnity, "\t\t{0}", "{");
                     foreach (MetaArgInfo argInfo in methodInfo.DetailArgs)
@@ -1086,14 +1305,14 @@ namespace ChromaSDK
                     {
                         Output(swUnity, "\t\t\tPlugin{0}({1});",
                             methodInfo.Name,
-                            RemoveArgTypes(methodInfo.Args, methodInfo.DetailArgs));
+                            RemoveArgTypes(methodInfo));
                     }
                     else
                     {
                         Output(swUnity, "\t\t\t{0} result = Plugin{1}({2});",
                             ChangeToManagedType(methodInfo.ReturnType),
                             methodInfo.Name,
-                            RemoveArgTypes(methodInfo.Args, methodInfo.DetailArgs));
+                            RemoveArgTypes(methodInfo));
                     }
 
                     foreach (MetaArgInfo argInfo in methodInfo.DetailArgs)
