787ea94f9d3afc82c9aa5ae0b40b7e3dadbbf582
Tim Graupmann <tgraupmann@gmail.com>
2019-01-17 09:21:39 -0800

    Change class name

--- bin/Debug/ChromaAnimationAPI.cs
+++ bin/Debug/ChromaAnimationAPI.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 
 namespace ChromaSDK
 {
-    public class UnityNativeChromaSDK
+    public class ChromaAnimationAPI
     {
 #if UNITY_3 || UNITY_3_0 || UNITY_3_1 || UNITY_3_2 || UNITY_3_3 || UNITY_3_4 || UNITY_3_5
         const string DLL_NAME = "UnityNativeChromaSDK3";
